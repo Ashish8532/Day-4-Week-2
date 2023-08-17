001e94da45c561ff36bba863afa10910797276e5
Ashish8532 <ashishraj916263@outlook.com>
2023-08-17 11:48:33 +0530

    adds exercise 1 for day 4 of week 2

---
+++ BlogSystem.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BlogSystem.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSystem.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .HasMany(c => c.Comments)
+                .WithOne(c => c.Blog)
+                .HasForeignKey(c => c.BlogId)
+                .HasPrincipalKey(c => c.Id);
+        }
+    }
+}
---
+++ BlogSystem.Models/Models/Comment.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogSystem.Models.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
